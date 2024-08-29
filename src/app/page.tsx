640e7ee6aac91fcd5b7926ddcb1b7a34f9156284
ArthurDevLeal <arthurdevleal@gmail.com>
2024-08-29 19:34:28 -0300

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,32 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Video from "@/components/Video";
+
 function Page() {
-	const [playing, setPlaying] = useState<boolean>(false);
+	const [clock, setClock] = useState("00:00:00");
+
+	useEffect(() => {
+		setInterval(() => {
+			const date = new Date();
+			const formattedDateTime = new Intl.DateTimeFormat("pt-BR", {
+				hour: "numeric",
+				minute: "numeric",
+				second: "numeric",
+				hour12: false, // Usa o formato 24 horas
+			}).format(date);
+			setClock(formattedDateTime);
+		}, 1000);
+
+	}, []);
+
 	return (
-		<div className="text-white">
-			<div className="border border-white p-3 mb-4">
-				<p className="text-2xl font-bold mb-2">{playing ? "Rodando" : "Pausado"}</p>
-				<button onClick={() => setPlaying(!playing)}  className="bg-white text-black p-2 rounded-md active:opacity-80">
-					Play/Pause
-				</button>
-			
+		<main className="h-full flex justify-center items-center">
+			<div className="bg-gray-900 w-auto inline-block p-6 rounded-xl shadow-lg">
+				<div className="text-5xl font-bold text-blue-400 font-mono flex justify-center items-center space-x-2">
+					<p className="text-blue-300">{clock}</p>
+				</div>
 			</div>
-			
-			<Video src={"https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"} isPlaying={playing} />
-		</div>
+		</main>
 	);
 }
 export default Page;
