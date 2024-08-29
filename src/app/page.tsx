b843744e842374ba140dbccb6c1fdfa15e0f5e9e
ArthurDevLeal <arthurdevleal@gmail.com>
2024-08-29 19:04:13 -0300

    videoplayer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import Video from "@/components/Video";
 function Page() {
-	return <div className="">...</div>;
+	const [playing, setPlaying] = useState<boolean>(false);
+	return (
+		<div className="text-white">
+			<div className="border border-white p-3 mb-4">
+				<p className="text-2xl font-bold mb-2">{playing ? "Rodando" : "Pausado"}</p>
+				<button onClick={() => setPlaying(!playing)}  className="bg-white text-black p-2 rounded-md active:opacity-80">
+					Play/Pause
+				</button>
+			
+			</div>
+			
+			<Video src={"https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"} isPlaying={playing} />
+		</div>
+	);
 }
 export default Page;
