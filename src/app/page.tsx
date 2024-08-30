b11625a9ccc19377af5bf46a1c67b729635d0ffb
ArthurDevLeal <arthurdevleal@gmail.com>
2024-08-30 14:38:39 -0300

    nothing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Video from "@/components/Video";
 function Page() {
 	const [playing, setPlaying] = useState<boolean>(false);
@@ -8,12 +8,11 @@ function Page() {
 		<div className="text-white">
 			<div className="border border-white p-3 mb-4">
 				<p className="text-2xl font-bold mb-2">{playing ? "Rodando" : "Pausado"}</p>
-				<button onClick={() => setPlaying(!playing)}  className="bg-white text-black p-2 rounded-md active:opacity-80">
+				<button onClick={() => setPlaying(!playing)} className="bg-white text-black p-2 rounded-md active:opacity-80">
 					Play/Pause
 				</button>
-			
 			</div>
-			
+
 			<Video src={"https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"} isPlaying={playing} />
 		</div>
 	);
