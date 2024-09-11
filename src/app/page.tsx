129049b27a2f76f4652c044e395d630362b83b11
ArthurDevLeal <arthurdevleal@gmail.com>
2024-09-11 16:08:05 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,6 @@ function page() {
 	return (
 		<main className="flex flex-col justify-center items-center">
 			<h1 className="text-white text-3xl font-bold">Chat Simples</h1>
-			<ChatProvider>
-				<Chat />
-			</ChatProvider>
 		</main>
 	);
 }
