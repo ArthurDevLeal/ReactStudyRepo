2917512fbbd1e62e855797c802ef34e26c1eb227
ArthurDevLeal <arthurdevleal@gmail.com>
2024-09-14 18:32:22 -0300

    axios post formData

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,23 @@
 "use client";
 
+import axios from "axios";
 import { useRef, useState } from "react";
 
 function Page() {
-	const [legendInput, setLegendInput] = useState("");
-	const fileInputRef = useRef<HTMLInputElement>(null);
-	const allowedImgs = ["image/jpeg", "image/png"];
-	const handeFileSend = async () => {
-		if (fileInputRef.current?.files && fileInputRef.current.files.length > 0) {
-			const fileItem = fileInputRef.current.files[0];
-			if (allowedImgs.includes(fileItem.type)) {
-				const data = new FormData();
-				data.append("img", fileItem);
-				data.append("legend", legendInput);
-				const req = await fetch("https://jsonplaceholder.typicode.com/posts", {
-					method: "POST",
-					headers: {
-						"Content-type": "multipart/form-data",
-					},
-					body: data,
-				});
-				const json = await req.json()
-				console.log(json);
-				
-			} else {
-				alert("Aqruivo incompativel");
-			}
+	const fileInput = useRef<HTMLInputElement>(null);
+	async function handleSendFile() {
+		if (fileInput.current?.files && fileInput.current.files.length > 0) {
+			const file = fileInput.current.files[0];
+			const data = new FormData();
+			data.append("name", "file");
+			data.append("file", file);
+			const req = await axios.post("https://jsonplaceholder.typicicode/posts", data);
 		}
-		alert("Selecione um arquivo por favor!");
-	};
+	}
 	return (
 		<div className="container mx-auto">
-			<h1 className="text-3xl">Upload de imagem</h1>
-
-			<div className="flex flex-col gap-3 max-w-md border border-dotted border-white p-3 mt-4">
-				<input type="file" ref={fileInputRef} />
-				<input
-					type="text"
-					placeholder="Digite uma legenda"
-					className="p-3 bg-white rounded-md text-black"
-					value={legendInput}
-					onChange={(e) => setLegendInput(e.target.value)}
-				/>
-				<button onClick={handeFileSend}>Enviar imagem</button>
-			</div>
+			<input ref={fileInput} type="file" />
+			<button onClick={handleSendFile}>Enviar</button>
 		</div>
 	);
 }
