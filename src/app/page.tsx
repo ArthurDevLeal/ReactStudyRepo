e06c00608fe392ef9f9014d74825894f41d8006e
ArthurDevLeal <arthurdevleal@gmail.com>
2024-09-27 20:23:21 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,9 @@
-"use client"
-import Clock from "@/components/Clock";
-import Greeting from "@/components/Greeting";
-import Rating from "@/components/Rating";
-import PersonList from "@/components/PersonList";
-import TodoList from "@/components/TodoList";
+"use client";
 function page() {
 	return (
-		
-		
-
-
-		// <TodoList/>
-		// <main className="bg-gradient-to-tr from-sky-400 to-indigo-400 w-full h-screen flex flex-col items-center justify-center">
-		// 	<h1 className="text-9xl text-white">
-		// 		<Clock />
-		// 	</h1>
-		// 	<p className="text-4xl text-white">
-		// 		<Greeting />
-		// 	</p>
-		// </main>
-
-		// <table className="container mx-auto text-white ">
-		// 	<thead className="px-2 bg-blue-950">
-		// 		<tr className="text-left">
-		// 			<th className="w-1/3 p-4">Name</th>
-		// 			<th className="p-4">Status</th>
-		// 			<th className="p-4">Grade 1</th>
-		// 			<th className="p-4">Grade 2</th>
-		// 			<th className="p-4">Final Grade</th>
-		// 		</tr>
-		// 	</thead>
-		// 	<tbody className="text-black">
-		// 		<PersonList />
-		// 	</tbody>
-		// </table>
+		<div>
+			<h1>A</h1>
+		</div>
 	);
 }
 
